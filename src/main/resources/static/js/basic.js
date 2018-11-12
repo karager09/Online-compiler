4b19e0b9148dd6505cd5c2f6bdaf22ef27d802b0
Paciata <patrycja.kopacz96@gmail.com>
2018-11-12 17:21:23 +0100

    Add some features

--- src/main/resources/static/js/basic.js
+++ src/main/resources/static/js/basic.js
@@ -1,6 +1,143 @@
-function sendCode()
-{
-    var code = $("#code").val();
+var editor = null;
+
+CodeMirror.commands.autocomplete = function(cm) {
+    cm.showHint({hint: CodeMirror.hint.anyword});
+}
+
+$(document).ready(function(){
+
+    var code = $(".codemirror-textarea")[0];
+    editor = CodeMirror.fromTextArea(code,{
+        lineNumbers : true,
+        lineWrapping: true,
+        addModeClass: true,
+        styleActiveLine: true,
+        matchBrackets: true,
+        autoCloseBrackets : true,
+        showTrailingSpace: true,
+        mode : "clike",
+        dragDrop : false,
+        fullScreen : false,
+        highlightSelectionMatches: {showToken: /\w/, annotateScrollbar: true},
+        extraKeys: {
+            "Esc": function(cm) {
+                if (cm.getOption("fullScreen")) cm.setOption("fullScreen", false);
+            },
+            "Ctrl-L": function(cm){
+                cm.foldCode(cm.getCursor());
+            },
+            "Alt-F": "findPersistent",
+            "Ctrl-Space": "autocomplete"
+        },
+        foldGutter: true,
+        gutters: ["CodeMirror-linenumbers", "CodeMirror-foldgutter"]
+    });
+
+    loadTheme();
+
+    var readOnlyLines = [0,1,2,3];
+    editor.on('beforeChange',function(cm,change) {
+        if ( ~readOnlyLines.indexOf(change.from.line) ) {
+            change.cancel();
+
+        }
+    });
+});
+
+function selectLanguage(){
+    var input = document.getElementById("selectLanguage");
+    // todo po zmienie zmien mode, tylko nie wiesz jakie jezyki wiec nie ma mode
+    // todo nic sie nie dzieje jeszcze XD
+    // setLanguage(language)
+}
+
+function setLanguage(language){
+    editor.setOption("mode", language);
+}
+
+function uploadFile() {
+    var file = document.getElementById("uploadedFile");
+
+    if ('files' in file) {
+        if (file.files.length != 0) {
+            var file = file.files[0];
+
+            if ('type' in file) {
+                setLanguage(file.type);
+            }
+
+            var reader = new FileReader();
+            reader.onload = function(e) {
+                var text = reader.result;
+                editor.setValue(text);
+            }
+            reader.readAsText(file);
+        }
+    }
+}
+
+function saveCodeToFile() {
+    // todo you can specify filename and data type and file extension
+    var text = editor.getValue();
+    var filename = "code.txt";
+
+    var element = document.createElement('a');
+    element.setAttribute('href', 'data:text/plain;charset=utf-8,' + encodeURIComponent(text));
+    element.setAttribute('download', filename);
+
+    element.style.display = 'none';
+    document.body.appendChild(element);
+    element.click();
+    document.body.removeChild(element);
+}
+
+function saveOutputToFile() {
+    // todo you can specify filename
+    var text = document.getElementById('compileResponse').value;
+    var filename = "output.txt";
+
+    var element = document.createElement('a');
+    element.setAttribute('href', 'data:text/plain;charset=utf-8,' + encodeURIComponent(text));
+    element.setAttribute('download', filename);
+
+    element.style.display = 'none';
+    document.body.appendChild(element);
+    element.click();
+    document.body.removeChild(element);
+}
+
+function selectTheme() {
+    var input = document.getElementById("selectTheme");
+    var theme = input.options[input.selectedIndex].textContent;
+    editor.setOption("theme", theme);
+    location.hash = "#" + theme;
+}
+
+function loadTheme() {
+    var choice = (location.hash && location.hash.slice(1)) || (document.location.search && decodeURIComponent(document.location.search.slice(1)));
+    var input = document.getElementById("selectTheme");
+
+    if (choice) {
+        input.value = choice;
+        editor.setOption("theme", choice);
+    }
+    CodeMirror.on(window, "hashchange", function() {
+        var theme = location.hash.slice(1);
+        if (theme) { input.value = theme; selectTheme(); }
+    });
+}
+function fullscreen(){
+    // todo add alert with information about closing fullscreen
+    if (editor.getOption("fullScreen")) {
+        editor.setOption("fullScreen", false);
+    } else {
+        editor.setOption("fullScreen", true);
+    }
+}
+
+function sendCode(){
+
+    var code = editor.getValue();
     var input = $("#input").val();
 
     $.ajax({
@@ -14,13 +151,8 @@ function sendCode()
             language: "Cpp",
             runCompiledProgram : false
         })
-
-
     }).then(function (data, status, jqxhr) {
 
         $("#compileResponse").val('Status success, Server response: \n' + data);
-
-
     });
-
 }
